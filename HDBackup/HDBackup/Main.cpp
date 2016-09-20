2ccf9176906b945919b6c81fe6812cb267d45601
Sam Huff <contact@samueljhuff.com>
2016-09-19 20:30:19 -0700

    Generate Timestamp

--- HDBackup/HDBackup/Main.cpp
+++ HDBackup/HDBackup/Main.cpp
@@ -5,21 +5,29 @@ using std::cin;
 using std::endl;
 
 #include <string>
+using std::string;
+using std::to_string;
 #include <ctime>
 using std::time;
 
 #include <Windows.h>
 
+string createTimestamp(tm t);
+
 int main(int argc, char *argv[])
 {
 	int out = 0;
 	char _source[64] = { 0 };
-	char _dest[64] = { 0 };
+	char _dest[64] = { "E" };
 	char _id[64] = { 0 };
+	string timestamp;
+	string mkdir;
 
 	time_t t = time(0);
 	struct tm now;
 	localtime_s(&now, &t);
+	timestamp = createTimestamp(now);
+
 
 	if (argc == 1)
 	{
@@ -29,7 +37,6 @@ int main(int argc, char *argv[])
 		cout << "Source drive letter: ";
 		cin.getline(_source, 64);
 		// End user input
-		
 	}
 	else if (argc == 2)
 	{
@@ -51,15 +58,22 @@ int main(int argc, char *argv[])
 			cout << "Version " << VERSION << endl;
 		}
 	}
-	else if (argc == 3)
-	{
-		cout << "User, src input" << endl;
-	}
 	else
 	{
 		cout << "ERROR: Incorrect parameters. Try HDBackup.exe /h for help." << endl;
 		out = 1;
 	}
+	return out;
+}
+
+string createTimestamp(tm t)
+{
+	string out;
+	out = to_string(t.tm_mon) + "/" + 
+		  to_string(t.tm_mday) + "/" + 
+		  to_string(t.tm_year) + "_" + 
+		  to_string(t.tm_hour) + ":" + 
+		  to_string(t.tm_min) + ":" + to_string(t.tm_sec);
 	return out;
 }
 
